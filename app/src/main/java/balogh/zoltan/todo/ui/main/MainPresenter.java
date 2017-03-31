c1984b8a509fccc0c0eaa5941249c8859d9ae4a4
Balogh Zoltán <balogh.zoltan.email@gmail.com>
2017-03-31 09:50:02 +0200

    Naive MVP implementation

---
+++ app/src/main/java/balogh/zoltan/todo/ui/main/MainPresenter.java
@@ -0,0 +1,28 @@
+package balogh.zoltan.todo.ui.main;
+
+import balogh.zoltan.todo.ui.Presenter;
+
+public class MainPresenter extends Presenter<MainScreen> {
+
+    private static MainPresenter instance = null;
+
+    private MainPresenter() {
+    }
+
+    public static MainPresenter getInstance() {
+        if (instance == null) {
+            instance = new MainPresenter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+}
---
+++ app/src/main/java/balogh/zoltan/todo/ui/main/MainScreen.java
@@ -0,0 +1,5 @@
+package balogh.zoltan.todo.ui.main;
+
+public interface MainScreen {
+    void showMessage(String text);
+}
